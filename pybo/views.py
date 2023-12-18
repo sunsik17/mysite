88a6e539e33771042e4b4bbf4ceff3f6d65c154a
sunsik17 <sunsig21c@gmail.com>
2023-12-18 21:27:20 +0900

    url 매핑 시키기 form데이터를 url에 사용하기 endpoint생성 방법
    장고 패턴 파악 완료
    
    기본적은 뷰는 django의 generic모듈을 이용하면 제너릭하게 사용할 수 있다.
    그러나 복잡한 케이스에서 더 어렵게 작성되는 경우가 종종 있으니 주의해야 한다.

--- pybo/views.py
+++ pybo/views.py
@@ -1,10 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 
 from .models import Question
 
 
 # Create your views here.
 def index(request):
-    questions = Question.objects.order_by('-create_date')  # - 가 있으면 내림차순 desc, 없으면 오름차순 asc
-    context = {'questions': questions}
-    return render(request, 'pybo/questions.html', context)
+    question_list = Question.objects.order_by('-create_date')  # - 가 있으면 내림차순 desc, 없으면 오름차순 asc
+    context = {'question_list': question_list}
+    return render(request, 'pybo/question_list.html', context)
+
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    context = {'question': question}
+    return render(request, 'pybo/question_detail.html', context)
+
+
+def answer_create(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    question.answer_set.create(content=request.POST.get('content'))
+    return redirect('pybo:detail', question_id=question_id)
