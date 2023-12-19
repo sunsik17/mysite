19d47bc2eab2bf8547f02beace917610f3154b84
sunsik17 <sunsig21c@gmail.com>
2023-12-19 17:12:24 +0900

    django의 forms 모듈을 사용하면 is_valid()함수가 있고 이는 null 체크를 한다.
    또한 as_p를 이용해 간편하게 프로퍼티를 p태그로 사용할 수 있지만,
    프로퍼티마다 다른 태그를 사용하거나, 부트스트랩 css적용에 대해 에러사항이 있어 직접 html문서를 수정하는 것을 진행했다.
    
    또한 as_p를 이용해도 widgets의 attr를 이용해 부트스트랩을 적용 할 클래스를 넘겨줄 수 있다.
    
    is_valid()를 통해 null체크를 진행하면 템플릿에서 오류에 대한 응답을 간편하게 내려줄 수 있다.

--- pybo/views.py
+++ pybo/views.py
@@ -1,6 +1,7 @@
+from django.http import HttpResponseNotAllowed
 from django.shortcuts import render, get_object_or_404, redirect
 
-from .forms import QuestionForm
+from .forms import QuestionForm, AnswerForm
 from .models import Question
 
 
@@ -19,10 +20,28 @@ def detail(request, question_id):
 
 def answer_create(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
-    question.answer_set.create(content=request.POST.get('content'))
-    return redirect('pybo:detail', question_id=question_id)
+    if request.method == 'POST':
+        form = AnswerForm(request.POST)
+        if form.is_valid():
+            answer = form.save(commit=False)
+            answer.question = question
+            answer.save()
+            return redirect('pybo:detail', question_id=question_id)
+    else:
+        return HttpResponseNotAllowed('Only \'POST\' method possible')
+    context = {'question': question, 'form': form}
+    return render(request, 'pybo/question_detail.html', context)
 
 
 def question_create(request):
-    form = QuestionForm()
-    return render(request, 'pybo/question_form.html', {'form': form})
+    if request.method == 'POST':
+        form = QuestionForm(request.POST)
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.save()
+            return redirect('pybo:index')
+    else:
+        form = QuestionForm()
+
+    context = {'form': form}
+    return render(request, 'pybo/question_form.html', context)
