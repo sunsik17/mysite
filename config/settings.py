4f901c90839c186db5c556478dec6725937e57b1
sunsik17 <sunsig21c@gmail.com>
2023-12-21 17:26:22 +0900

    login / logout 기능은 게시판 서비스에서만 사용할 것이 아니므로 common app을 생성해서 구현
    django에 이미 login/logout 기능이 구현돼 있어 url 매핑, 로그인 및 로그아웃 성공시 redirect url만 지정해주면 간단하게 기능이 구현된다.

--- config/settings.py
+++ config/settings.py
@@ -26,15 +26,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
-
 # Application definition
 
 INSTALLED_APPS = [
-    'common.apps.CommonConfig'
+    'common.apps.CommonConfig',
     'pybo.apps.PyboConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -117,8 +112,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
