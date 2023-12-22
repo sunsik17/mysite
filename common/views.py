c0cd6ca830740f6e7cfb920f8628097abc9c15c3
sunsik17 <sunsig21c@gmail.com>
2023-12-22 17:16:56 +0900

    login redirect 는 되는데 logout redirect가 되지 않고 GET http://localhost:8000/common/logout 요청이 가서 405에러 리스폰 반환 --- 장고 5.0을 사용중이었음 장고 4.x버전에는 LogoutView.asView()로 간단하게 로그아웃을 구현했었음 나는 해당 함수로 리다이렉트가 안돼서 2시간넘게 안되고있었는데 auth에서 logout함수가 있는걸 발견 해당 함수를 활용해 logout을 만들었고 index로 직접 redirect 시킴; from django.contrib.auth import logout 에서 찾아보니 logout함수가 구현돼있었고..
    # common/views.py
    from django.contrib.auth import logout as auth_logout
    
    def logout(request):  # 추가
        auth_logout(request)
        return redirect('index')
    
    ------------------------
    # common/urls.py
    app_name = 'common'
    
    urlpatterns = [
    ...
        path('logout/', views.logout, name='logout'), #  추가
    ...
    ]
    해서 해결함

--- common/views.py
+++ common/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout as auth_logout
+from django.shortcuts import render, redirect
+
+from common.forms import SignUpForm
+from pybo import views
+
 
 # Create your views here.
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)  # 인증
+            login(request, user)  # 로그인
+            return redirect('index')
+    else:
+        form = SignUpForm()
+
+    return render(request, 'common/signup.html', {'form': form})
+
+
+def logout(request):
+    auth_logout(request)
+    return redirect('index')
